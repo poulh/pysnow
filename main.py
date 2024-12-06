ff74d0db6a4be6dd25ba9ff8b6914efc8a8273a5
Hornsleth, Poul <Poul.Hornsleth@mlp.com>
2024-12-06 17:09:54 -0500

    updated requirements.txt

---
+++ main.py
@@ -0,0 +1,31 @@
+
+import pysnow
+# import pandas as pd
+print('hello')
+
+
+
+if __name__ == "__main__":
+    dict = {"sys_id": [], "name": []}
+
+    client = pysnow.Client(
+        instance="mpgops",
+        user="svc_vision_snow",
+        password="er8Z(YZW",
+    )
+
+    locations_table_path = "/table/cmn_location"
+    location_resource = client.resource(api_path=locations_table_path)
+    location_centers = location_resource.get(
+        path="/table/cmn_location",
+        query=pysnow.QueryBuilder().field("sys_id").not_equals(""),
+        fields=["sys_id", "name"],
+        limit=50000,
+    ).all()
+
+    for location_center in location_centers:
+        print(location_center)
+        dict["sys_id"].append(location_center["sys_id"])
+        dict["name"].append(location_center["name"])
+
+print(dict)
